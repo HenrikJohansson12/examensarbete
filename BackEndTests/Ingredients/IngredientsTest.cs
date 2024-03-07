a1e1df98bf3481988fe816d2698740cc8c0c0473
Henrik Johansson <henrik.johansson@easycom.com>
2024-03-07 14:18:36 +0100

    Testing to read in ingredients from livsmedelsverket.

---
+++ BackEndTests/Ingredients/IngredientsTest.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Text.Json;
+using Database.Models;
+
+namespace BackEndTests.Ingredients;
+using Database.Models.Livsmedelsverket;
+public class IngredientsTest
+{
+    [Fact]
+    public async void Can_CreateIngredient_rows_FromLivsmedelsverket_Api()
+    {
+        var httpClient = new HttpClient();
+
+        var apiResponse = await 
+            httpClient.GetAsync(
+                " https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel?offset=0&limit=10&sprak=1");
+
+        string jsonData = await apiResponse.Content.ReadAsStringAsync();
+        
+        Root? root =
+            JsonSerializer.Deserialize<Root>(jsonData);
+
+        var listOfIngredients = new List<Ingredient>();
+
+        foreach (var livsmedel in root.livsmedel)
+        {
+            //Lägg till här konverteringen. 
+        }
+    }
+}
---
+++ Database/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿namespace Database.Models;
+
+public class Ingredient
+{
+        public int IngredientId { get; set; }
+        public string Type { get; set; }
+        public int Number { get; set; }
+        public DateTime Version { get; set; }
+        public string Name { get; set; }
+}
