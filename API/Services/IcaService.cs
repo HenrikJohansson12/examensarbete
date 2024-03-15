a951b1b601c82706303b9eb0fd543a17c9d4f013
Henrik Johansson <henrik.johansson@easycom.com>
2024-03-15 13:56:31 +0100

    ICA tests successful.

--- API/Services/IcaService.cs
+++ API/Services/IcaService.cs
@@ -1,4 +1,5 @@
 using System.Data.SqlTypes;
+using System.Globalization;
 using API.Models;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -22,7 +23,10 @@ public class IcaService : IIcaService
     {
         _httpClient = httpClient;
     }
-    
+    public List<ProductRecord> GetRecords()
+    {
+        return productRecords;
+    }
     public async Task<bool> GetDiscountedProducts(GetDiscountedItemsIcaRequest req)
     {
         var productList = new List<Product>();
@@ -62,7 +66,11 @@ public class IcaService : IIcaService
                         decorateProductIds = dividedLists[i][j] + ",";
                     }
 
-                    decorateProductIds = decorateProductIds + dividedLists[i][j] + ",";
+                    else 
+                    {
+                        decorateProductIds = decorateProductIds + dividedLists[i][j] + ",";
+                    }
+                   
                 }
 
                 string productResponse = await _httpClient.GetStringAsync(
@@ -92,6 +100,11 @@ public class IcaService : IIcaService
             offerType = (int)(OfferType.MultiBuyOffer);
         }
 
+        if ((product.offer.description.Contains("för")))
+        {
+            offerType = (int)(OfferType.MultiBuyOffer);
+        }
+
        else if (product.offer.description.Contains("st"))
         {
             offerType = (int)(OfferType.PerProduct);
@@ -107,38 +120,12 @@ public class IcaService : IIcaService
         string brand = product.brand;
 
         decimal price;
-        if (decimal.TryParse(product.price.original?.amount, out price)== false)
-        {
-            price = 0;
-        }
+        price = Convert.ToDecimal(product.price.original?.amount, new CultureInfo("en-US"));
+       
         
         var minItems = 0;
         decimal discountedPrice = 0;
-        //Finding the discounted price based on the offertype. 
-        switch (offerType)
-        {   //Per product
-            case 1:
-                discountedPrice = Convert.ToDecimal(product.price.current.amount);
-                break;
-            //Per kg
-            case 2:
-                string perkgPattern = @"(\d+)\s*kr";
-                Match perKgMatch = Regex.Match(product.offer.description,perkgPattern);
-                if (perKgMatch.Success)
-                {
-                    discountedPrice = decimal.Parse(perKgMatch.Groups[1].Value);
-                }
-                break;
-            //MultiOffer
-            case 3:
-                string multiOfferPattern = @"för\s+(\d+)\s+kr";
-                Match multiOfferMatch = Regex.Match(product.offer.description,multiOfferPattern);
-                if (multiOfferMatch.Success)
-                {
-                    discountedPrice = decimal.Parse(multiOfferMatch.Groups[1].Value);
-                }
-                break;
-        }
+     
         
         decimal quantity = 0;
         string quantityUnit = "";
@@ -146,15 +133,29 @@ public class IcaService : IIcaService
         Match match = Regex.Match(product.size.value, pattern);
         if (match.Success)
         {
-            quantity = decimal.Parse(match.Groups[1].Value.Replace(',', '.'));
-            quantityUnit = match.Groups[3].Value.Trim();
+            quantity = Convert.ToDecimal(match.Groups[1].Value.Replace(',', '.'),new CultureInfo("en-US"));
+            if (match.Groups[3].Value.Trim() == "")
+            {
+                quantityUnit =  product.size.uom.ToLower();
+            }
+
+            if (quantityUnit=="")
+            {
+               quantityUnit = match.Groups[3].Value.ToLower();
+            }
         }
         
         //Ibland kan vara en min items kampanj men variabeln är inte satt. 
         if (product.offer.requiredProductQuantity!= null)
         {
-            minItems = product.offer.requiredProductQuantity.Value;
+            minItems = Convert.ToInt32(product.offer.requiredProductQuantity.Value);
         }
+        if (minItems == 0 && offerType == Convert.ToInt32(OfferType.MultiBuyOffer))
+        {
+            Match minItemsMatch = Regex.Match(product.offer.description, @"^(\d+)");
+            minItems = Convert.ToInt32(minItemsMatch.Groups[1].Value);
+        }
+     
 
         bool isMemberOffer = product.offer.description.Contains("Stammis");
         int maxItems = 0;
@@ -167,6 +168,36 @@ public class IcaService : IIcaService
             {
                 maxItems = Convert.ToInt32(maxMatch.Groups[1].Value);
             }
+        }
+        
+        //Finding the discounted price based on the offertype. 
+        switch (offerType)
+        {   //Per product
+            case 1:
+                discountedPrice = Convert.ToDecimal(product.price.current.amount,new CultureInfo("en-US"));
+                break;
+            //Per kg
+            case 2:
+                string perkgPattern = @"(\d+)\s*kr";
+                Match perKgMatch = Regex.Match(product.offer.description,perkgPattern);
+                if (perKgMatch.Success)
+                {
+                    discountedPrice = decimal.Parse(perKgMatch.Groups[1].Value);
+                    price = decimal.Round((1000 / quantity) * price);
+                    
+                }
+                break;
+            //MultiOffer
+            case 3:
+                string multiOfferPattern = @"för\s+(\d+)\s+kr";
+                Match multiOfferMatch = Regex.Match(product.offer.description,multiOfferPattern);
+                if (multiOfferMatch.Success)
+                {
+                    discountedPrice = Convert.ToDecimal(multiOfferMatch.Groups[1].Value,new CultureInfo("en-US"));
+                    discountedPrice = decimal.Round(discountedPrice/minItems,2);
+                }
+                price = Convert.ToDecimal(product.price.current?.amount, new CultureInfo("en-US"));
+                break;
         }
         
         DateTime date = DateTime.Now; 
@@ -187,7 +218,6 @@ public class IcaService : IIcaService
             Name = name,
             OfferType = offerType,
             Brand = brand,
-            Description = "",
             Price = price,
             DiscountedPrice = discountedPrice,
             Quantity = quantity,
---
+++ BackEndTests/IcaTest/IcaServiceUnitTest.cs
@@ -0,0 +1,186 @@
+﻿using System.Net;
+using Moq;
+using System.IO;
+using API.Properties.Services;
+using API.Requests;
+using Database.Models;
+
+namespace BackEndTests;
+
+public class IcaServiceUnitTest
+{
+    public class IcaHttpMessageHandlerMock : HttpMessageHandler
+    {
+        private readonly HttpResponseMessage[] _responses;
+        private int _count = 0;
+        private readonly HttpResponseMessage _response;
+        private readonly HttpStatusCode _code;
+        
+        public IcaHttpMessageHandlerMock(HttpStatusCode code)
+        {
+            _code = code;
+        }
+      
+        public IcaHttpMessageHandlerMock( params HttpResponseMessage[] responses)
+        {
+            _responses = responses;
+        }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (_responses!= null)
+            {
+                var response = _responses[_count++ % _responses.Length];
+                return Task.FromResult(response);
+
+            }
+            if (_response!= null)
+            {
+                return Task.FromResult(_response);
+
+            }
+            return Task.FromResult(new HttpResponseMessage()
+            {
+                StatusCode = _code,
+            });
+        }
+    }
+    [Fact]
+    public async Task Ica_Products_Are_Correctly_Converted()
+    {
+        string firstResponse = File.ReadAllText("Icaresponse.json");
+        string response2 = File.ReadAllText("Decorate1.json");
+        string response3 = File.ReadAllText("Decorate2.json");
+        string response4 = File.ReadAllText("Decorate3.json");
+        string response5 = File.ReadAllText("Decorate4.json");
+        var http = new HttpClient( new IcaHttpMessageHandlerMock(
+            new HttpResponseMessage()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(firstResponse)
+        }, new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(response2),
+            }, new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(response3),
+            }, new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(response4),
+            }, new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(response5),
+            }
+            
+            ));
+        var icaService = new IcaService(http);
+        var request = new GetDiscountedItemsIcaRequest { StoreId = 1 };
+        // Test. 
+        await  icaService.GetDiscountedProducts(request);
+
+        var result = icaService.GetRecords();
+        // Index 0
+        var perKiloGramOffer = new ProductRecord()
+        {
+            OfferType = 2,
+            Name = "Äpple Ingrid Marie ca 190g Klass 1 ICA",
+            Brand = "ICA",
+            Price = 35.0m,
+            DiscountedPrice = 25.0m,
+            Quantity = 190,
+            QuantityUnit = "g",
+            MinItems = 0,
+            MaxItems = 0,
+            IsMemberOffer = false,
+            //Not good for testing. 
+            StartDate = new DateOnly(2024, 3, 11),
+            EndDate = new DateOnly(2024, 3, 17),
+
+        };
+        // Index 2
+        var multiOffer = new ProductRecord()
+        {
+            OfferType = 3,
+            Name = "Mellanmjölkdryck 1,5% Laktosfri 1,5l Arla Ko®",
+            Brand = "Arla Ko",
+            Price = 21.90m,
+            DiscountedPrice = 16.67m,
+            Quantity = 1.5m,
+            QuantityUnit = "l",
+            MinItems = 3,
+            MaxItems = 1,
+            IsMemberOffer = false,
+            StartDate = new DateOnly(2024, 3, 11),
+            EndDate = new DateOnly(2024, 3, 17),
+
+        };
+        // Index 4
+        var perProductOffer = new ProductRecord()
+        {
+            OfferType = 1,
+            Name = "Röd spetsig paprika 200g Klass 1 ICA",
+            Brand = "ICA",
+            Price = 24.90m,
+            DiscountedPrice = 20m,
+            Quantity = 0.2m,
+            QuantityUnit = "kg",
+            MinItems = 0,
+            MaxItems = 0,
+            IsMemberOffer = false,
+            StartDate = new DateOnly(2024, 3, 11),
+            EndDate = new DateOnly(2024, 3, 17),
+
+        };
+        
+        var multiOffer2 = new ProductRecord()
+        {
+            OfferType = 3,
+            Name = "Skinka Basturökt 120g Lönneberga",
+            Brand = "Lönneberga",
+            Price = 29.90m,
+            DiscountedPrice = 20m,
+            Quantity = 0.12m,
+            QuantityUnit = "kg",
+            MinItems = 2,
+            MaxItems = 0,
+            IsMemberOffer = true,
+            StartDate = new DateOnly(2024, 3, 11),
+            EndDate = new DateOnly(2024, 3, 17),
+
+        };
+
+        Assert.Equivalent(perKiloGramOffer,result[0]);
+        Assert.Equivalent(multiOffer,result[2]);
+        Assert.Equivalent(perProductOffer,result[4]);
+        Assert.Equivalent(multiOffer2,result[28]);
+        
+        
+    }
+  
+}
+
+public class ServiceTest
+{
+    private readonly HttpClient _http;
+
+    public ServiceTest(HttpClient http)
+    {
+        _http = http;
+    }
+    public async Task<string> CreateAsync()
+    {
+        var response = await _http.GetAsync("https://google.com");
+        if (response.StatusCode == HttpStatusCode.BadRequest)
+        {
+            return null;
+        }
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
+    
+    
+   
